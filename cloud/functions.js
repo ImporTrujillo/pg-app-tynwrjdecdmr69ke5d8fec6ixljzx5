b66822e65e06608134e0ac3219359ef76f3a4c06
ImporTrujillo <35789178+ImporTrujillo@users.noreply.github.com>
2024-05-10 17:02:58 -0500

    Update functions.js

--- cloud/functions.js
+++ cloud/functions.js
@@ -4,5 +4,5 @@
 
 Parse.Cloud.define('hello', function (request, response)
 {
-  response.success("Hello from SashiDo's Simple Cloud Code :)");
+  response.success("Hello from SashiDo's Simple Cloud Codes :)");
 });
